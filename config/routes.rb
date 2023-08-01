1b61b0890571d07e7d2c4df2124e59158b5b6cfc
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-01 05:49:35 +0000

    [Add]User詳細画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
-
-
   get "home/about" => "homes#about", as: 'about'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :update, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

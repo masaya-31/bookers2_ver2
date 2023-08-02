dd8a179fc92a19aaf344e70678ae2090f5981771
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-02 08:00:57 +0000

    [Add]フラッシュメッセージの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book)
     else
       @user = current_user
@@ -30,6 +31,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -39,6 +41,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "You have destroyed book successfully."
     redirect_to books_path
   end
 

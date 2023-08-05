d1b76c6cde645d0615e8b89a193ef0c75f1f9f0a
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-05 06:45:59 +0000

    いいね機能のファイル準備完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :update, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

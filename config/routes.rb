4952ba97643c9415a96e363745d986437518bffc
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-05 05:16:57 +0000

    コメント投稿機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'homes#top'
   get "home/about" => "homes#about", as: 'about'
 
-  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:index, :show, :update, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

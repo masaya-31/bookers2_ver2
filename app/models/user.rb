5ea12837b31b1fa9aef6e84e3de6c314eb6f1caf
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-08-07 01:43:20 +0000

    [Add]フォロー・フォロワー機能の実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,18 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # 自分がフォローする側の関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :following, through: :relationships, source: :followed
+  # 自分がフォローされる側の関係
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
 
   has_one_attached :profile_image
-
   validates :name, uniqueness: true, length: { in: 2..20 }
-
   validates :introduction, length: { maximum: 50 }
-
 
   def get_profile_image(width, height)
     unless profile_image.attached?
@@ -22,4 +27,20 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
   end
+
+  # フォローするとき
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  # フォローを外すとき
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  # フォローしているか確認するとき
+  def following?(user)
+    following.include?(user)
+  end
+
 end
